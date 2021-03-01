d2a2b63ee3a922f6d626db13abcaee6ad010e87f
despar <despar@microsoft.com>
2021-03-01 10:22:09 -0800

    setting up app

--- DereksProjects/DereksProjects/settings.py
+++ DereksProjects/DereksProjects/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'RandomWordGen',
+    #'RandomWordGen',
+    'NinjaGold',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
